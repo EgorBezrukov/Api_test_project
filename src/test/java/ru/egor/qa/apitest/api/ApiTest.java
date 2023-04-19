6cfa3c60e5e8e149717b16fb4b8dbbc07741b4f6
EgorBezrukov <poladez@yandex.ru>
2023-04-19 17:59:03 +0300

    Refactor
    Рефакторинг кода

--- src/test/java/ru/egor/qa/apitest/api/ApiTest.java
+++ src/test/java/ru/egor/qa/apitest/api/ApiTest.java
@@ -1,5 +1,6 @@
 package ru.egor.qa.apitest.api;
 
+import io.qameta.allure.Description;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Assert;
@@ -9,36 +10,47 @@ import java.util.List;
 
 public class ApiTest {
 
-    private final String reqres = "https://reqres.in/api/";
-    //    Проверка статус-кода ответа:
+    private final String reqResBaseUrl = "https://reqres.in/";
+
     @Test
+    @Description("роверка статус-кода ответа")
     public void testStatusCode() {
-        Response response = RestAssured.given().log().all().get(reqres + "users?page=2");
-        int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 200);
+        Response response = RestAssured.
+                given().baseUri(reqResBaseUrl).
+                log().all().
+                get("api/users?page=2");
+        Assert.assertEquals(response.getStatusCode(),200);
     }
 
-    //    Проверка значения поля "page" в ответе:
     @Test
+    @Description("Проверка значения поля \"page\" в ответе")
     public void testPageValue() {
-        Response response = RestAssured.given().log().all().get(reqres + "users?page=2");
-        int pageValue = response.jsonPath().getInt("page");
-        Assert.assertEquals(pageValue, 2);
+        Response response = RestAssured.
+                given().baseUri(reqResBaseUrl).
+                log().all().
+                get("api/users?page=2");
+        Assert.assertEquals(response.jsonPath().getInt("page"), 2);
     }
 
-    //    Проверка количества элементов в массиве "data":
     @Test
+    @Description("Проверка количества элементов в массиве \"data\"")
     public void testDataArraySize() {
-        Response response = RestAssured.given().log().all().get(reqres + "users?page=2");
+        Response response = RestAssured.
+                given().baseUri(reqResBaseUrl).
+                log().all().
+                get("api/users?page=2");
         List<Object> dataArray = response.jsonPath().getList("data");
         Assert.assertEquals(dataArray.size(), 6);
     }
 
-    //    Проверка значения поля "id" у первого элемента массива "data":
     @Test
+    @Description("Проверка значения поля \"id\" у первого элемента массива \"data\"")
     public void testFirstDataId() {
-        Response response = RestAssured.given().log().all().get(reqres + "users?page=2");
-        int firstDataId = response.jsonPath().getInt("data[0].id");
-        Assert.assertEquals(firstDataId, 7);
+        Response response = RestAssured.
+                given().baseUri(reqResBaseUrl).
+                log().all().
+                get("users?page=2");
+        Assert.assertEquals(response.jsonPath().getInt("data[0].id"), 7);
     }
+
 }
